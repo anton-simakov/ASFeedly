c8b444c50d73ee3a575832a4fc710eea21ee3c6e
Anton Simakov <asimakov@reviversoft.com>
2015-08-05 00:50:40 +0300

    Clean up properties

--- ASFFeedly/ASFCredential.h
+++ ASFFeedly/ASFCredential.h
@@ -10,21 +10,17 @@
 
 @interface ASFCredential : NSObject<NSCoding>
 
-@property(nonatomic, strong) NSString *userID;
 @property(nonatomic, strong) NSString *refreshToken;
 @property(nonatomic, strong) NSString *accessToken;
 @property(nonatomic, strong, readonly) NSDate *expiration;
 @property(nonatomic, strong) NSString *tokenType;
-@property(nonatomic, strong) NSString *plan;
-@property(nonatomic, strong) NSString *state;
 
 @property (readonly, nonatomic, assign, getter = isExpired) BOOL expired;
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
 + (void)storeCredential:(ASFCredential *)credential;
-+ (ASFCredential *)retrieveCredential;
 
-- (void)setExpiresIn:(long)ti;
++ (ASFCredential *)retrieveCredential;
 
 @end
--- ASFFeedly/ASFCredential.m
+++ ASFFeedly/ASFCredential.m
@@ -9,13 +9,10 @@
 #import "ASFCredential.h"
 
 static NSString *const kCodeKey           = @"code";
-static NSString *const kUserIDKey         = @"userID";
 static NSString *const kRefreshTokenKey   = @"refreshToken";
 static NSString *const kAccessTokenKey    = @"accessToken";
 static NSString *const kExpirationKey     = @"expiration";
 static NSString *const kTokenTypeKey      = @"tokenType";
-static NSString *const kPlanKey           = @"plan";
-static NSString *const kStateKey          = @"state";
 
 static NSString *const ASFCredentialKey = @"ASFCredential";
 
@@ -44,38 +41,26 @@ static NSString *const ASFCredentialKey = @"ASFCredential";
     self = [super init];
     if (self)
     {
-        _userID         = [coder decodeObjectForKey:kUserIDKey];
         _refreshToken   = [coder decodeObjectForKey:kRefreshTokenKey];
         _accessToken    = [coder decodeObjectForKey:kAccessTokenKey];
         _expiration     = [coder decodeObjectForKey:kExpirationKey];
         _tokenType      = [coder decodeObjectForKey:kTokenTypeKey];
-        _plan           = [coder decodeObjectForKey:kPlanKey];
-        _state          = [coder decodeObjectForKey:kStateKey];
     }
     return self;
 }
 
 - (void)encodeWithCoder:(NSCoder *)coder
 {
-    [coder encodeObject:_userID         forKey:kUserIDKey];
     [coder encodeObject:_refreshToken   forKey:kRefreshTokenKey];
     [coder encodeObject:_accessToken    forKey:kAccessTokenKey];
     [coder encodeObject:_expiration     forKey:kExpirationKey];
     [coder encodeObject:_tokenType      forKey:kTokenTypeKey];
-    [coder encodeObject:_plan           forKey:kPlanKey];
-    [coder encodeObject:_state          forKey:kStateKey];
 }
 
 - (NSString *)description
 {
-    return [NSString stringWithFormat:
-            @"UserID: %@, refresh token: %@, access token: %@, expiration: %@, token type: %@, plan: %@, state: %@",
-            _userID, _refreshToken, _accessToken, _expiration, _tokenType, _plan, _state];
-}
-
-- (void)setExpiresIn:(long)ti
-{
-    [self setExpiration:[NSDate dateWithTimeIntervalSinceNow:ti]];
+    return [NSString stringWithFormat:@"Refresh token: %@, access token: %@, expiration: %@, token type: %@",
+            _refreshToken, _accessToken, _expiration, _tokenType];
 }
 
 - (BOOL)isExpired {
